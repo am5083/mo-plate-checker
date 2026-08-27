2f44d9ce07a479177c2ba63d436a570fe43baea4
Ahmed Mohamed <contact@ahmh.dev>
2026-08-27 00:53:02 -0500

    update model in tests using helper function `modelAfter : String -> Main.Model`
    This is done to simulate page update; genius

--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -27,44 +27,39 @@ import Test.Html.Selector as Selector
 --
 
 
+modelAfter : String -> Main.Model
+modelAfter str =
+    Main.update (Main.SeedChange str) Main.init
+
+
 suite : Test
 suite =
     describe "Main"
         [ describe "correctly render normalized seed '  ahmed  '"
             [ test "renders normalized seed + value" <|
                 \_ ->
-                    Main.view _
+                    Main.view (modelAfter "AHMED")
                         |> Query.fromHtml
                         |> Query.has [ Selector.text "Normalized seed: AHMED" ]
-
-
             , test "render at least \"AHMEDD\"" <|
                 \_ ->
-                    Main.view _
+                    Main.view (modelAfter "AHMED")
                         |> Query.fromHtml
                         |> Query.has [ Selector.text "AHMEDD" ]
-
-
             , test "updating with invalid characters shows invalid-character method" <|
                 \_ ->
-                    let msg = Main.update (Main.SeedChange "AH!!ED") in
-                            Main.view _
-                                |> Query.fromHtml
-                                |> Query.has [ Selector.text "invalid character" ]
-
-
+                    Main.view (modelAfter "AH!!ED")
+                        |> Query.fromHtml
+                        |> Query.has [ Selector.text "invalid character" ]
             , test "update with empty seed returns empty-plate message" <|
-                    let msg = Main.update (Main.SeedChange "AH!!ED") in
-                            Main.view _
-                                |> Query.fromHtml
-                                |> Query.has [ Selector.text "plate is empty" ]
-
-
+                \_ ->
+                    Main.view (modelAfter "")
+                        |> Query.fromHtml
+                        |> Query.has [ Selector.text "plate is empty" ]
             , test "calling viewVariations (Ok []) renders the no-candidates message" <|
                 \_ ->
-                    let msg =
-                            Main.viewVariations (Ok [])_
-                                |> Query.fromHtml
-                                |> Query.has [ Selector.text "no candidates exist" ]
+                    Main.viewVariations (Ok [])
+                        |> Query.fromHtml
+                        |> Query.has [ Selector.text "no candidates exist" ]
             ]
         ]
